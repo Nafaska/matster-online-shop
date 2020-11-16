af430ed5f77cbb19c04295cb46ec4dfdc5bd0ead
Nafaska <viktoria.kovalenko7@gmail.com>
2020-11-16 23:05:42 +0100

    added sorting

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { ReactComponent as Logo } from "../assets/Logo.svg";
 import { ReactComponent as Bag } from "../assets/Bag.svg";
-import { changeCurrency } from "../reducers/goods";
+import { changeCurrency, setSort } from "../reducers/goods";
 
-const EUR  = 'EUR';
-const CAD = 'CAD';
-const USD = 'USD';
-const NAME = 'NAME';
-const PRICE = 'PRICE';
-
+const EUR = "EUR";
+const CAD = "CAD";
+const USD = "USD";
+const NAME = "title";
+const PRICE = "price";
 
 const Header = () => {
   const history = useHistory();
+  const [sortButtonClicked, setSortButtonClicked] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -23,11 +23,13 @@ const Header = () => {
 
   const sortList = (sortMethod) => {
     console.log(sortMethod);
-  }
+    setSortButtonClicked(!sortButtonClicked);
+    return dispatch(setSort(sortMethod, sortButtonClicked));
+  };
 
   const changeCurrencyOnClick = (currency) => {
-    return dispatch(changeCurrency(currency))
-  }
+    return dispatch(changeCurrency(currency));
+  };
 
   return (
     <div>
@@ -47,13 +49,13 @@ const Header = () => {
               onClick={() => sortList(NAME)}
               className="block mt-4 lg:inline-block lg:mt-0 text-white mr-4"
             >
-              Name
+              Sort By Name {sortButtonClicked ? "Z→A" : "A→Z"}
             </button>
             <button
               onClick={() => sortList(PRICE)}
               className="block mt-4 lg:inline-block lg:mt-0 text-white mr-4"
             >
-              Price
+              Sort by Price {sortButtonClicked ? "High→Low" : "Low→High"}
             </button>
           </div>
           <div className="text-sm lg:flex-grow">
--- client/src/reducers/goods.js
+++ client/src/reducers/goods.js
@@ -2,12 +2,15 @@ import axios from "axios";
 
 const GET_ALL_GOODS = "GET_ALL_GOODS";
 const CHANGE_CURRENCY = "CHANGE_CURRENCY";
+const SORT_BY_NAME = "SORT_BY_NAME";
 const USD = "USD";
 
 const initialState = {
   list: [],
   rate: 1,
   currency: USD,
+  method: "",
+  order: false,
 };
 
 export default (state = initialState, action) => {
@@ -23,6 +26,25 @@ export default (state = initialState, action) => {
         currency: action.data,
         rate: action.rate,
       };
+    case SORT_BY_NAME:
+      let arr = [...state.list];
+      let sortedList = sortBy(arr, action.method);
+      if (state.order) {
+        return {
+          ...state,
+          list: sortedList.reverse(),
+          method: action.method,
+          order: action.order
+        };
+      } else {
+        return {
+          ...state,
+          list: sortedList,
+          method: action.method,
+          order: action.order,
+        };
+      }
+
     default:
       return state;
   }
@@ -45,8 +67,28 @@ export function changeCurrency(currency) {
       dispatch({
         type: CHANGE_CURRENCY,
         rate: data.rates[currency],
-        data: currency
-      })
+        data: currency,
+      });
     });
+  };
+}
+
+const sortBy = (data, method) => {
+  return data.sort((a, b) => {
+    if (a[method] > b[method]) {
+      return 1;
+    }
+    if (b[method] > a[method]) {
+      return -1;
+    }
+    return 0;
+  });
+};
+
+export function setSort(method, order) {
+  return {
+    type: SORT_BY_NAME,
+    method: method,
+    order: !order,
   };
 }
